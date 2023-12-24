2568e0fff86222752a77cb4ab15746acfe321948
Jean <acalto2@gmail.com>
2023-12-24 19:27:49 +0100

    66. Server-side form validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"bed_brkfst/internal/config"
+	"bed_brkfst/internal/forms"
 	"bed_brkfst/internal/models"
 	"bed_brkfst/internal/render"
 	"encoding/json"
@@ -55,7 +56,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 
 // Reservation renders the make a reservation page and displays form
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+// PostReservation handles the posting of a reservation form
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 }
 
 // Generals renders the room page
