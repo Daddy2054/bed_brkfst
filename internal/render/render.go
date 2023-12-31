14425a2e93a6e210633ff42599403946472d4c2b
Jean <acalto2@gmail.com>
2023-12-31 17:02:35 +0100

    102. Cleaning up our code

--- internal/render/render.go
+++ internal/render/render.go
@@ -1,15 +1,16 @@
 package render
 
 import (
+	"bed_brkfst/internal/config"
+	"bed_brkfst/internal/models"
 	"bytes"
 	"errors"
 	"fmt"
-	"github.com/justinas/nosurf"
-	"bed_brkfst/internal/config"
-	"bed_brkfst/internal/models"
 	"html/template"
 	"net/http"
 	"path/filepath"
+
+	"github.com/justinas/nosurf"
 )
 
 var functions = template.FuncMap{}
@@ -18,7 +19,7 @@ var app *config.AppConfig
 var pathToTemplates = "./templates"
 
 // NewTemplates sets the config for the template package
-func NewTemplates(a *config.AppConfig) {
+func NewRenderer(a *config.AppConfig) {
 	app = a
 }
 
@@ -31,8 +32,8 @@ func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateDa
 	return td
 }
 
-// RenderTemplate renders a template
-func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) error {
+// Template renders a template
+func Template(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) error {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
