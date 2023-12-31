d717febe779988d1b0bbe163bbf47ec4c89e18fc
Jean <acalto2@gmail.com>
2023-12-31 20:57:43 +0100

    107. Searching for availability by room

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,14 @@
 package repository
 
-import "bed_brkfst/internal/models"
+import (
+	"bed_brkfst/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 }
