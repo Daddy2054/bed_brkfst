a9aaef0b6fb06ee051678328de6f094d4772a75d
Jean <acalto2@gmail.com>
2024-01-04 19:18:29 +0100

    118. Adding a migration for seeding restrictions

---
+++ migrations/20240104174743_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO restrictions ( restriction_name, created_at, updated_at)
+VALUES ('Reservation','2023-12-31 00:00:00','2023-12-31 00:00:00'),
+('Owner block','2024-01-01 00:00:00','2024-01-01 00:00:00'  );
