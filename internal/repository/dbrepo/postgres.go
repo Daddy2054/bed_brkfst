3abae30111b2c5522e2e39b0cd8f40158704979d
Jean <acalto2@gmail.com>
2024-01-10 19:34:52 +0100

    156. Reservation Calendar II

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -505,7 +505,15 @@ func (m *postgresDBRepo) AllRooms() ([]models.Room, error) {
 
 	var rooms []models.Room
 
-	query := `select id, room_name, created_at, updated_at from rooms order by room_name`
+	query := `
+		SELECT 
+			id, 
+			room_name, 
+			created_at, 
+			updated_at 
+		FROM rooms 
+		ORDER by room_name
+		`
 
 	rows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
